f500678a402272898ebad3446e22b8ee18cbcced
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2018-05-11 21:20:26 +0200

    Remove weblate remembers

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,76 +1,5 @@
 #!/bin/bash
 
-current_version="2.20"
-
-ynh_check_global_uwsgi_config () {
-	uwsgi --version || ynh_die "You need to add uwsgi (and appropriate plugin) as a dependency"
-
-	if [ -f /etc/systemd/system/uwsgi-app@.service ];
-	then
-		echo "Uwsgi generic file is already installed"
-	else
-		cp ../conf/uwsgi-app@.socket /etc/systemd/system/uwsgi-app@.socket
-		cp ../conf/uwsgi-app@.service /etc/systemd/system/uwsgi-app@.service
-	fi
-
-	# make sure the folder for sockets exists and set authorizations
-	mkdir -p /var/run/uwsgi/
-	chown root:www-data /var/run/uwsgi/
-	chmod -R 775 /var/run/uwsgi/
-
-	# make sure the folder for logs exists and set authorizations
-	mkdir -p /var/log/uwsgi/app/
-	chown root:www-data /var/log/uwsgi/app/
-	chmod -R 775 /var/log/uwsgi/app/
-}
-
-# Create a dedicated uwsgi ini file to use with generic uwsgi service
-# It will install generic uwsgi.socket and
-#
-# This will use a template in ../conf/uwsgi.ini
-# and will replace the following keywords with
-# global variables that should be defined before calling
-# this helper :
-#
-#   __APP__       by  $app
-#   __PATH__      by  $path_url
-#   __FINALPATH__ by  $final_path
-#
-# usage: ynh_add_systemd_config
-#
-# to interact with your service: `systemctl <action> uwsgi-app@app`
-ynh_add_uwsgi_service () {
-	ynh_check_global_uwsgi_config
-
-	# www-data group is needed since it is this nginx who will start the service
-	usermod --append --groups www-data "$app" || ynh_die "It wasn't possible to add user $app to group www-data"
-
-	finaluwsgiini="/etc/uwsgi/apps-available/$app.ini"
-	ynh_backup_if_checksum_is_different "$finaluwsgiini"
-	cp ../conf/uwsgi.ini "$finaluwsgiini"
-
-	# To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
-	# Substitute in a nginx config file only if the variable is not empty
-	if test -n "${final_path:-}"; then
-		ynh_replace_string "__FINALPATH__" "$final_path" "$finaluwsgiini"
-	fi
-	if test -n "${path_url:-}"; then
-		ynh_replace_string "__PATH__" "$path_url" "$finaluwsgiini"
-	fi
-	if test -n "${app:-}"; then
-		ynh_replace_string "__APP__" "$app" "$finaluwsgiini"
-	fi
-	ynh_store_file_checksum "$finaluwsgiini"
-
-	chown root: "$finaluwsgiini"
-	systemctl enable "uwsgi-app@$app.socket"
-	systemctl start "uwsgi-app@$app.socket"
-	systemctl daemon-reload
-
-	# Add as a service
-	yunohost service add "uwsgi-app@$app.socket" --log "/var/log/uwsgi/app/$app"
-}
-
 # Remove the dedicated uwsgi ini file
 #
 # usage: ynh_remove_systemd_config
@@ -87,42 +16,6 @@ ynh_remove_uwsgi_service () {
 	fi
 }
 
-
-weblate_fill_settings() {
-	settings="$1"
-
-	ynh_replace_string "__NAME__"       "$app"            "$settings"
-	ynh_replace_string "__DB_PWD__"     "$db_pwd"         "$settings"
-	ynh_replace_string "__ADMIN__"      "$admin"          "$settings"
-	ynh_replace_string "__ADMINMAIL__"  "$admin_mail"     "$settings"
-	ynh_replace_string "__DOMAIN__"     "$domain"         "$settings"
-	ynh_replace_string "__KEY__"        "$key"            "$settings"
-	ynh_replace_string "__FINALPATH__"  "$final_path"     "$settings"
-	ynh_replace_string "__MEMCPORT__"   "$memc_port"      "$settings"
-	ynh_replace_string "__GITHUBUSER__" "$github_account" "$settings"
-
-	# root install as an empty PATHURL to prevent '//static'
-	if [ "$path_url" == "/" ]
-	then
-		ynh_replace_string "__PATHURL__" "" "$settings"
-	else
-		ynh_replace_string "__PATHURL__" "$path_url" "$settings"
-	fi
-}
-
-ynh_check_if_checksum_is_different() {
-	local file=$1
-	local checksum_setting_name=checksum_${file//[\/ ]/_}	# Replace all '/' and ' ' by '_'
-	local checksum_value=$(ynh_app_setting_get $app $checksum_setting_name)
-	local check=0
-
-	if ! echo "$checksum_value $file" | md5sum -c --status
-	then	# If the checksum is now different
-		check=1
-	fi
-
-	echo "$check"
-}
 
 #=================================================
 #
